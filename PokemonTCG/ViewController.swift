99e334a0adb343bea3605403916f04ba759583c7
Sergio Bravo <sergio.bravo@monstar-lab.com>
2021-08-03 14:32:55 -0500

    Initial Commit

---
+++ PokemonTCG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonTCG
+//
+//  Created by Sergio Bravo on 3/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
